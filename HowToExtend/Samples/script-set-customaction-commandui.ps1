7f066195bbfd0702da1a4e6ba6d755d7269e406a
sleonard <shawnadrockleonard@gmail.com>
2017-08-04 02:10:02 -0400

    adding commandui sample

---
+++ HowToExtend/Samples/script-set-customaction-commandui.ps1
@@ -0,0 +1,13 @@
+﻿
+# Connect to site
+$siteurl = "https://[tenant].sharepoint.com/sites/[siteurl]"
+
+Connect-SPIaC -CredentialName "sponline" -Url $siteurl
+
+
+# Read the XML file and add/update Custom Actions
+Set-IaCCustomActionByXml -Identity "List Title" -XmlFilePath "c:\filedir\sample.xml" -Verbose
+
+
+# Close the connection
+Disconnect-SPIaC
