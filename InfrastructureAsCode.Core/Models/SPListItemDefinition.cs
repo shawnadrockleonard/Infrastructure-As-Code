598ce6306cafd184b057b32f5a67833c87be73f0
Shawn Leonard <sleonard@microsoft.com>
2017-11-16 13:00:24 -0700

    updating model with additional attributes

--- InfrastructureAsCode.Core/Models/SPListItemDefinition.cs
+++ InfrastructureAsCode.Core/Models/SPListItemDefinition.cs
@@ -14,16 +14,44 @@ namespace InfrastructureAsCode.Core.Models
             this.RoleBindings = new List<SPPrincipalModel>();
         }
 
+        /// <summary>
+        /// The title column for the listitem
+        /// </summary>
         public string Title { get; set; }
 
+        /// <summary>
+        /// The date the item was created
+        /// </summary>
         public Nullable<DateTime> Created { get; set; }
 
+        /// <summary>
+        /// User who created the item
+        /// </summary>
         public SPPrincipalUserDefinition CreatedBy { get; set; }
 
+        /// <summary>
+        /// The date the item was last modified
+        /// </summary>
         public Nullable<DateTime> Modified { get; set; }
 
+        /// <summary>
+        /// User who last modified the item
+        /// </summary>
         public SPPrincipalUserDefinition ModifiedBy { get; set; }
 
+        /// <summary>
+        /// The URL to the Item
+        /// </summary>
+        public string FileRef { get; set; }
+
+        /// <summary>
+        /// The URL to the folder containing the Item
+        /// </summary>
+        public string FileDirRef { get; set; }
+
+        /// <summary>
+        /// Contains the ListItem Column data
+        /// </summary>
         public List<SPListItemFieldDefinition> ColumnValues { get; set; }
 
         /// <summary>
